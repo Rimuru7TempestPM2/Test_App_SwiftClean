02679bbb7c475b2879e30fe1a5ab6de5a53aee44
Tempest_Software <ngothopeter46@gmail.com>
2026-03-28 15:27:00 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'swiftclean-v2';
+const ASSETS = [
+  'index.html',
+  'config.js',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
